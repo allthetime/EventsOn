489869ca8f3a46ac36eded4b20a7ee9f4d629a79
Christopher Reid <chronic88@gmail.com>
2014-09-24 15:47:00 -0700

    adding signin functions

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,27 @@
 get '/' do
   erb :index
 end
+
+helpers do
+  @chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
+  def salt_string
+    (0...50).map { @chars[rand(@chars.length)] }.join
+  end
+end
+
+post '/signup' do
+  salted_and_hashed_password = Digest::SHA256.hexdigest(params[:password]+salt_string)
+  @user = User.new(
+    name: params[:name],
+    password: password
+  )
+  if @user.save
+    redirect '/'
+  else
+    erb :signup
+  end
+end
+
+post '/login'
+
+end
