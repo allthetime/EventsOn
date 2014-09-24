6bce4d66f7402e482aa9ae73911ffa3e0807216d
Veronika Bitkina <veronika@zisel.ca>
2014-09-24 16:09:31 -0700

    event changes website

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,17 @@
 get '/' do
   erb :index
 end
+
+get '/events/new' do
+  @event = Event.new
+  erb :'events/new'
+end
+
+post '/events/new' do
+  @event = Event.new(
+    name: params[:name],
+    description:  params[:description],
+    link_url: params[:link_url]
+  )
+  @event.save
+end
