d9d1b86d21e37afd34ff11cab0ddbbf62210ff58
Veronika Bitkina <veronika@zisel.ca>
2014-09-24 14:56:28 -0700

    event_planner change

--- app/models/event_planner.rb
+++ app/models/event_planner.rb
@@ -1,5 +1,13 @@
-class EventPlanner < User
+class EventPlanner < ActiveRecord::Base
+    validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :salt, presence: true
 
+
+  has_many :comments
+  has_many :reviews
+  has_many :bookmarks
   has_many :events
 
 end
