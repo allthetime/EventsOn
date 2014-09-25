c78cc503dba787d49e8682da180f4cdc8d1bb538
Veronika Bitkina <veronika@zisel.ca>
2014-09-24 19:55:52 -0700

    event changes

--- app/actions.rb
+++ app/actions.rb
@@ -8,12 +8,18 @@ get '/events/new' do
   erb :'events/new'
 end
 
-post '/events/new' do
+post '/events' do
   @event = Event.new(
     name: params[:name],
     description:  params[:description],
     link_url: params[:link_url],
-    picture_url: params[:picture_url]
+    picture_url: params[:picture_url],
+    date: Time.new(params[:date]),
+    venue_id: 1
   )
-  @event.save
+  if @event.save
+    redirect "/events/#{@event.id}"
+  else
+    erb :'event/new'
+  end
 end
